ddbb1e0078129b7bc13ca5e26b86a1f566014856
Drifter <megadrifter@gmail.com>
2017-12-07 15:51:19 +0300

    working test with code style conventions used. Pending Allure

--- src/main/java/challenge/App.java
+++ src/main/java/challenge/App.java
@@ -1,59 +1,10 @@
 package challenge;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        String myFile = "C:\\Temp\\file.csv";
-        // readTheFile(myFile);
     }
-
-    /*
-    private static void readTheFile(String fileFullPath)
-    {
-
-        List<Object> dataList = new ArrayList<>();
-
-        // need to check if fileFullPath is not empty
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(
-                        new FileInputStream(fileFullPath), StandardCharsets.UTF_8))){
-            String line;
-            while ((line = reader.readLine()) != null)
-            {
-
-                String[] myVarsArray = line.split(";");
-                for (String x: myVarsArray) {System.out.println(x);}
-                dataList.add(line);
-
-//                System.out.println(line);
-
-            }
-            for (Object x: dataList)
-            {
-                System.out.println(x);
-            }
-
-
-        } catch (IOException e) {
-            // log error
-        }
-    }
-    */
-
-    /*
-    private void readTheFileLambda(String fileFullPath)
-    {
-        Files.readAllLines(Paths.get(fileFullPath), StandardCharsets.UTF_8).forEach(System.out::println);
-    }
-    */
 }
--- src/test/java/challenge/OperationTest.java
+++ src/test/java/challenge/OperationTest.java
@@ -1,3 +1,10 @@
+/*
+ * OperationTest
+ *
+ * @version 12/2017
+ *
+ * @author Andrey Sentsov
+ */
 package challenge;
 
 import org.junit.Test;
@@ -10,81 +17,113 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+/**
+ * Класс для юнит (JUnit) тестов арифметических действий.
+ * По заданию потенциального работодателя.
+ * Тип данных принят int, поскольку не указано иное.
+ */
 @RunWith(Parameterized.class)
 public class OperationTest {
+    /* Класс проводит юнит тест на основе данных в файле, путь к которому в нём
+     жестко прописан. Файл должен лежать в ресурсах теста и называться file.csv */
 
-    @Parameterized.Parameters(name = "{index}: {0}{2}{1}={3}")
-    public static Iterable<Object[]> data() {
-        List<Object[]> dataList = new ArrayList<>();
+    // Все комментарии приведены для себя на время разработки
 
-        /* // присвоение вручную работает верно
-        Object[] row1 = {"0", "0", "+", "0"};
-        dataList.add(row1);
-        Object[] row2 = {"0", "0", "+", "0"};
-        dataList.add(row2);
-        System.out.println("dataList=" + dataList);
-        */
-
-        // Присвоение из файла:
-        String myFile = "C:\\Temp\\file.csv";
-        System.out.println("Test data:");
-
-        // открыть и считать строки
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(
-                        new FileInputStream(myFile), StandardCharsets.UTF_8))) {
-            String line;
-            // читаю строку
-            while ((line = reader.readLine()) != null) {
-                // разбиваю на элементы
-                Object[] myVarsArray = line.split(";");
-                for (Object x : myVarsArray) {
-                    System.out.print(x.toString() + "; ");
-                } // вывод на экран
-                System.out.println();
-                // добавляю массив в список
-                dataList.add(myVarsArray);
-
-            }
-            //       // проверка содержимого
-            //        for (Object x: dataList)
-            //        {
-            //            System.out.println(x.toString());
-            //        }
-
-            // так было в примере чтения
-        } catch (IOException e) {
-            // log error
-        }
-
-
-        return dataList;
-// return Arrays.asList(new Object[][]{{"0", "0", "+", "0"}, {"1", "1", "+", "2"}, {"1", "2", "+", "2"}});
-    }
-
+    // сначала переменные
     private String operand1;
     private String operand2;
     private String operation;
     private String expected;
 
-    public OperationTest(String operand1, String operand2, String operation, String expected) {
+    // затем конструктор
+    public OperationTest(String operand1, String operand2,
+                         String operation, String expected) {
         this.operand1 = operand1;
         this.operand2 = operand2;
         this.operation = operation;
         this.expected = expected;
     }
 
-    @Test
-    public void test() {
-        // convert args here
+    // потом методы
+    // параметры для параметризированного теста и заголовок
+    @Parameterized.Parameters(name = "{index}: Проверка операции {0} {2} {1} = {3}")
+    public static Iterable<Object[]> dataForTest() {
+        String myFile = "src\\test\\resources\\file.csv"; // путь к файлу
+        List<Object[]> dataList = new ArrayList<>();      // Список с данными
+        Object[] myVarsArray;                             // элемент списка с данными
+        String line;                                      // для считывания строки
 
-        // chose operation with swtich-case or if-else
+        System.out.println("Test data:");                 // для меня
 
-        assertEquals(expected, operand1 + operand2);
+        // открыть и считать строки. Основано на примере открытия файла.
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(myFile), StandardCharsets.UTF_8))) {
+            /* читаю строку */
+            while ((line = reader.readLine()) != null) {
+                /* разбиваю на элементы */
+                myVarsArray = line.split(";");
+                for (Object x : myVarsArray) {            // для меня
+                    System.out.print(x.toString() + "; ");
+                }
+                System.out.println();                     // для меня
+                /* добавляю массив в список */
+                dataList.add(myVarsArray);
+            }
+        } catch (IOException e) {   // так было в примере чтения.
+            // Прямой задачи обработки исключений не поставлено, пока не делаю.
+            // log error
+        }
+        return dataList;
     }
+
+    // потом сам тест
+    @Test
+    public void firstTest() {
+        /* входные данные сразу преобразовать в int. Нужна ли проверка, что там
+        именно числа? */
+        int intOperand1 = Integer.parseInt(operand1);
+        int intOperand2 = Integer.parseInt(operand2);
+        int intExpected = Integer.parseInt(expected);
+
+        assertEquals(intExpected,doOperation(intOperand1,intOperand2,operation));
+    }
+
+    /**
+     * Метод выполняет операцию oper над операндами op1, op2
+     * @param op1
+     * @param op2
+     * @param oper
+     * @return
+     */
+    private static int doOperation(int op1, int op2,String oper) {
+        int result = 0;
+
+        switch (oper) {            // лучше бы использовать чар?
+            case "+":
+                result = op1 + op2;
+                break;
+
+            case "-":
+                result = op1 - op2;
+                break;
+
+            case "*":
+                result = op1 * op2;
+                break;
+
+            case "/":
+                result = op1 / op2;
+                break;
+
+            default:
+                System.out.println("not valid operation");
+        }
+        return result;
+    }
+
 }
